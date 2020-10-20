c57d1393abb4c6d57249f7c14ab67ef23be6f0df
Daniel Rodriguez <danielrs9705@gmail.com>
2020-10-20 17:04:07 +0200

    Add configuration of the project

---
+++ src/main/java/com/globallabs/pots/PotsApplication.java
@@ -0,0 +1,23 @@
+package com.globallabs.pots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class PotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotsApplication.class, args);
+	}
+
+	@GetMapping(value="/hello")
+	public String getHello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+	
+}
---
+++ src/test/java/com/globallabs/pots/PotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globallabs.pots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
