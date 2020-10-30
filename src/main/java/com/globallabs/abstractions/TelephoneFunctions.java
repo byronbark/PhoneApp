7272d5d8eed9c60a1b5b2afe5b590234bad96cb6
Daniel Rodriguez <danielrs9705@gmail.com>
2020-10-30 16:50:11 +0100

    Add throws Dialing exception to the dial function

--- src/main/java/com/globallabs/abstractions/TelephoneFunctions.java
+++ src/main/java/com/globallabs/abstractions/TelephoneFunctions.java
@@ -1,5 +1,6 @@
 package com.globallabs.abstractions;
 
+import com.globallabs.phoneexceptions.DialingMySelfException;
 import com.globallabs.pots.Telephone;
 
 public interface TelephoneFunctions {
@@ -7,12 +8,19 @@ public interface TelephoneFunctions {
 	/**
 	 * Dial a phone from the current device
 	 * @param phoneToCall
+	 * @throws DialingMySelfException if the phone you are dialing is you
 	 */
-	public void dial(final int number);
+	public void dial(final int number) throws DialingMySelfException;
 	
 //	public void ring(final int phoneNumber);
 //	
 //	public void answer();
 //	
 //	public void hangUp();
+	
+	/**
+	 * Gets the info basic information
+	 * @return Telephone object
+	 */
+	public Telephone getPhoneInfo();
 }
