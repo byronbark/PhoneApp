97a04a68e30a8f9b29e3bee0ba221dfb2224cb87
Daniel Rodriguez <danielrs9705@gmail.com>
2020-10-30 16:49:14 +0100

    Add a new status when the phone is dialing

--- src/main/java/com/globallabs/pots/Status.java
+++ src/main/java/com/globallabs/pots/Status.java
@@ -1,7 +1,14 @@
 package com.globallabs.pots;
 
+/**
+ * The different status that a phone can
+ * be
+ * @author danielrs
+ *
+ */
 public enum Status {
 	BUSY,
 	OFF_CALL,
-	RINGING
+	RINGING,
+	DIALING
 }
